67ebff97752e4232efca0a72f67b2a3a136fcdcf
Matheus Rufca <matheusrufca@gmail.com>
2019-02-04 03:08:22 -0200

    Added sicredi-dragons enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCC6BU5ufoN-frSuAs2j_5LZA7ZHVpaXTY",
-    authDomain: "pwa-dummy-boilerplate.firebaseapp.com",
-    databaseURL: "https://pwa-dummy-boilerplate.firebaseio.com",
-    projectId: "pwa-dummy-boilerplate",
-    storageBucket: "pwa-dummy-boilerplate.appspot.com",
-    messagingSenderId: "250182784680"
-
+    apiKey: "AIzaSyBLpPcDdHOS4DJK-xRq_RNpTocTE5Xa30c",
+    authDomain: "sicredi-dragons.firebaseapp.com",
+    databaseURL: "https://sicredi-dragons.firebaseio.com",
+    projectId: "sicredi-dragons",
+    storageBucket: "sicredi-dragons.appspot.com",
+    messagingSenderId: "1033616918012"
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,23 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCC6BU5ufoN-frSuAs2j_5LZA7ZHVpaXTY",
-    authDomain: "pwa-dummy-boilerplate.firebaseapp.com",
-    databaseURL: "https://pwa-dummy-boilerplate.firebaseio.com",
-    projectId: "pwa-dummy-boilerplate",
-    storageBucket: "pwa-dummy-boilerplate.appspot.com",
-    messagingSenderId: "250182784680"
+    apiKey: "AIzaSyBLpPcDdHOS4DJK-xRq_RNpTocTE5Xa30c",
+    authDomain: "sicredi-dragons.firebaseapp.com",
+    databaseURL: "https://sicredi-dragons.firebaseio.com",
+    projectId: "sicredi-dragons",
+    storageBucket: "sicredi-dragons.appspot.com",
+    messagingSenderId: "1033616918012"
   }
 };
-
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
